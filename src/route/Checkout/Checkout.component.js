4794e62c3e320da2e719916530b7b8ec0cf04d18
Balázs Burján <bigbali141@gmail.com>
2021-12-01 20:01:56 +0200

    Forgot to remove test code

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -6,11 +6,6 @@ import './Checkout.override.style.scss'
 /** @namespace Route/Checkout/Component */
 export class Checkout extends SourceCheckout {
     render() {
-        this.stepMap.BILLING_STEPX = this.stepMap.BILLING_STEP
-        this.stepMap.BILLING_STEPY = this.stepMap.BILLING_STEP
-        this.stepMap.BILLING_STEPZ = this.stepMap.BILLING_STEP
-
-
         return (
             <>
                 <CheckoutProgress
